c37a8bf6b320cf55c0ab8f19986e41fe31471b9b
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-05-13 15:27:47 +0200

    client connect rework pool to client

--- app/utils/clientConnect.js
+++ app/utils/clientConnect.js
@@ -7,18 +7,16 @@ const debug = require("debug")("3db: clientConnect");
  * @description - connect to database
  * @returns {object} - database connection
  */
-const { Pool } = require("pg");
-const pool = new Pool({
-  
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-  
+const { Client } = require("pg");
+const client = new Client({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PG_DATABASE,
+  port: process.env.PGPORT
 });
-pool.connect();
+client.connect();
 
-/**
- * @description - export pool
- */
-module.exports = pool;
+
+
+module.exports = client;
