d4b234df31e777368d5b7c4907df3ccb5691b8fd
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-05-13 10:23:57 +0200

    separation of client and server, server will stay on this repo

--- FRONT/src/assets/baseApi.js
+++
@@ -1,5 +0,0 @@
-
-const baseApi = "https://projet-biblio-3d-production.up.railway.app/api"
-
-
-export default baseApi
--- FRONT/src/utils/BufferToUrl.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function bufferToUrl(buffer) {
-    const pics = new Uint8Array(buffer);
-    const blob = new Blob([pics], { type: "image/png" });
-    return URL.createObjectURL(blob);
-}
