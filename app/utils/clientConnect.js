35f622853bb8a690b17c5145c91e17759cce0e8a
TiboPaulJack <thibault.Garrigues@icloud.com>
2023-05-13 15:56:37 +0200

    pool to client in datamappers

--- app/utils/clientConnect.js
+++ app/utils/clientConnect.js
@@ -1,4 +1,3 @@
-// require dotenv
 require("dotenv").config();
 const debug = require("debug")("3db: clientConnect");
 
@@ -6,10 +5,17 @@ const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-client.connect();
+client.connect()
+  .then(() => {
+    debug("Connected to database");
+  })
+  .catch((error) => {
+    debug("Error connecting to database:", error);
+  });
 
 module.exports = client;
-
